a5be15047fffe5303e2dc68a6fc4a1e20077ec37
ImranAhmed26 <17-35401-3@student.aiub.edu>
2023-07-10 03:18:13 +0600

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
+import Head from "next/head";
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Metaversus</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://stijndv.com" />
+        <link
+          rel="stylesheet"
+          href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,38 @@
-import Head from "next/head";
-import FrontPage from "~/components/frontPage";
-import HomePage from "~/components/homePage";
+import React, { useState } from "react";
+import { Footer, Navbar } from "../components";
+import {
+  About,
+  Explore,
+  Feedback,
+  GetStarted,
+  Hero,
+  Insights,
+  WhatsNew,
+  World,
+} from "../sections";
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Indeed Clone</title>
-        <meta name="description" content="Software engineer jobs in dhaka bangladesh" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <HomePage />
-      </main>
-    </>
-  );
-}
+const Home = () => (
+  <div className="overflow-hidden bg-black/80">
+    <Navbar />
+    <Hero />
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0" />
+      <Explore />
+    </div>
+    <div className="relative">
+      <GetStarted />
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
+    </div>
+    <World />
+    <div className="relative">
+      <Insights />
+      <div className="gradient-04 z-0" />
+      <Feedback />
+    </div>
+    <Footer />
+  </div>
+);
+
+export default Home;
